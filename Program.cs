ce99e3ecf6c02301e31644403ef2fc48863b92d1
brucethedwarf <bruceleeshepardson@gmail.com>
2025-03-28 14:16:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace arrays_shepardson_bruce
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a whole number for a array size");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] numbers= new int[arraySize];
+            int sum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Enter a whole number to add to the array");
+                numbers[i] =Convert.ToInt32(Console.ReadLine());    
+                sum += numbers[i];
+            }
+            Console.WriteLine($"the sum of the array is {sum}");
+         Array.Sort( numbers );
+             Console.WriteLine("Array");
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+            
+         
+    }     
+}
